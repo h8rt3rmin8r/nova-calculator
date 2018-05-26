91480baffd3e4ad338eaa0d763252861ef02931c
William Thompson <willinspire@users.noreply.github.com>
2018-05-26 17:09:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #! /bin/bash
-
-# Install nova calculator
-# Created by h8rt3rmin8r on 20180520
+#----------------#
+#  Install nova  #
+#----------------#
+#------------------------------------------------------------------------------#
+# Source:        https://
+# Source Code:   https://github.com/resonova/nova-calculator/archive/master.zip
+# Created by:    h8rt3rmin8r
+# Date:          20180520
+#------------------------------------------------------------------------------#
 
 # Establish variables
-SOURCE1=${HOME}'/.nova/nova-calculator-master/'
-SOURCE2='/usr/local/bin/'
-NOVASOURCE1=${HOME}'/.nova/nova-calculator-master/nova-source/'
-NOVASOURCE2='/usr/local/bin/nova-source/'
 SOURCE_CODE='https://github.com/resonova/nova-calculator/archive/master.zip'
-TODAY="`TZ=America/New_York date '+%Y%m%d' | head -c -1`"
+TODAY="`date '+%Y%m%d' | head -c -1`"
+NOW="`date '+%Y%m%d-%H%M%S' | head -c -1`"
+SOURCE=${HOME}'/.software/nova/'
+SOURCE1=${HOME}'/.software/nova/nova-calculator-master/'
+SOURCE2='/usr/local/bin/'
+NOVASOURCE1=${HOME}'/.software/nova/nova-calculator-master/nova-source/'
+NOVASOURCE2='/usr/local/bin/nova-source/'
 
 # Verify root access
 uid=`id -u $USERNAME`
@@ -17,14 +25,17 @@ uid=`id -u $USERNAME`
 if [ "$uid" == "0" ]
     then
         echo ""
-        echo "install.sh: User is root"
-        echo "            Beginning installation process..."
+        echo "INSTALL: User is root"
+        echo "         Beginning installation process..."
         echo ""
     else
         echo ""
-        echo "install.sh: You need to have root access, run again with 'sudo'"
+        echo "INSTALL: You need to have root access, run again with 'sudo'"
     exit 1
 fi
+echo ""
+echo ${NOW}" -- Installation process started..."
+echo ""
 
 # Update the package list and install system updates
 apt-get update
@@ -40,6 +51,9 @@ apt-get install -y wget
 # INSTALL: dos2unix
 apt-get install -y dos2unix
 
+# INSTALL: neofetch
+apt-get install -y neofetch
+
 # INSTALL: unzip
 apt-get install -y unzip
 
@@ -51,8 +65,10 @@ apt-get update
 
 # Setup core files and construct directories
 cd ${HOME}/
-mkdir -p ${HOME}/.nova
-cd ${HOME}/.nova/
+mkdir -p ${HOME}/.software/
+mkdir -p ${HOME}/.software/nova/
+mkdir -p /usr/local/bin/nova-source/
+cd ${HOME}/.software/nova/
 
 # Download the nova-calculator repository archive
 wget --timeout=10 -q ${SOURCE_CODE}
@@ -60,14 +76,10 @@ wget --timeout=10 -q ${SOURCE_CODE}
 # Unzip the downloaded archive
 unzip -qq master.zip
 
-# Enter the new directory and make source scripts executable
+# Enter the new directory and make/install
 cd ${SOURCE1}
 mv ${SOURCE1}nova.sh ${SOURCE1}nova
-chmod +x ${SOURCE1}nova
-chmod +x ${NOVASOURCE1}nova.py
-
-# Make the nova-source bin location
-mkdir -p /usr/local/bin/nova-source/
+chmod +x ${SOURCE1}nova && chmod +x ${NOVASOURCE1}nova.py
 
 # Move source files into '/usr/local/bin/' and '/usr/local/bin/nova-source/'
 mv ${SOURCE1}nova ${SOURCE2}nova
@@ -77,17 +89,20 @@ mv ${NOVASOURCE1}PRINT ${NOVASOURCE2}PRINT
 mv ${NOVASOURCE1}README ${NOVASOURCE2}README
 mv ${NOVASOURCE1}VERSIONS ${NOVASOURCE2}VERSIONS
 
-# POSSIBLE BUGS EXIST IN ACCESS PROVISIONS
-#chmod -R 777 /usr/local/bin
-
 # Clean up the workspace and generate error log
-rm -rf ${HOME}/.nova/
-mkdir -p ${HOME}/.nova/; mkdir -p ${HOME}/.nova/logs/
-touch ${HOME}/.nova/logs/${TODAY}.log
-USER="`whoami`"; sudo chown -R ${USER}:${USER} ${HOME}/.nova
+rm -rf ${SOURCE}
+mkdir -p ${SOURCE}
+mkdir -p ${SOURCE}logs/
+touch ${SOURCE}logs/${TODAY}.log
+USER="`whoami`"
+sudo chown -R ${USER}:${USER} ${HOME}/.software/
 
 # Exit installation script
-echo ${TODAY}" -- Installation complete!" > ${HOME}/.nova/logs/${TODAY}.log
+echo ${TODAY}" -- Installation complete!" > ${HOME}/.software/nova/logs/${TODAY}.log
+echo ""
+echo ${NOW}" -- Installation complete!"
+echo ""
+nova -h
 exit
 
 ################################################################################
